1c72cdf3c89487cbbfad5a23c64ccf97b99c2432
one2four <907128347@qq.com>
2022-04-06 15:22:09 +0800

    Default Changelist

--- 01-test.py
+++ 01-test.py
@@ -1,11 +1,14 @@
-from selenium import webdriver
-import time
+# from selenium import webdriver
+# import time
 
-def main():
-    b = webdriver.Chrome()
-    b.get('https://www.baidu.com')
-    time.sleep(5)
-    b.quit()
+# def main():
+#     b = webdriver.Chrome()
+#     b.get('https://www.baidu.com')
+#     time.sleep(5)
+#     b.quit()
 
-if __name__ == '__main__':
-	 main()
+# if __name__ == '__main__':
+# 	 main()
+
+months = [31,28,31,30,31,30,31,31,30,31,30,31]
+print(months[1])
